4c751f5ae4cbfee5b4c02242b1554df49ca64ee3
Zlr- <25877692+zlr0001@users.noreply.github.com>
2025-08-26 12:42:26 +0200

    Add files via upload

---
+++ randomize/pkg/randomize_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const toggle: (a: number) => void;
+export const set_interval: (a: number, b: number) => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
